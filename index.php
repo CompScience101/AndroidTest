a25f15d5825f99ba62eaf8303f56b3ab76348f18
CompScience101 <35551293+CompScience101@users.noreply.github.com>
2018-01-21 23:21:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
        
     $jsonData      = array();
     $jsonTempData  = array();
-      
+    $word = "test";  
      for($i=1;$i<4; $i++)
        {
           $jsonTempData = array();
-          $jsonTempData['name']         = $data.$i;
-          $jsonTempData['number']       = $data.$i;
-          $jsonTempData['date_added']   = $data.$i;
+          $jsonTempData['name']         = $data.$i.$word;
+          $jsonTempData['number']       = $data.$i.$word;
+          $jsonTempData['date_added']   = $data.$i.$word;
            
           $jsonData[] = $jsonTempData;
        }
