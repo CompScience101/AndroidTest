228bc83194a8ce47355386d7e9d3997b03fa45cc
CompScience101 <35551293+CompScience101@users.noreply.github.com>
2018-01-21 23:45:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,4 @@
-<?php 
- 
-    // Get Post Data
-    $data = urldecode($_POST['data']);
-       
-    $jsonData      = array();
-    $jsonTempData  = array();
-    $word = "test";  
-     for($i=1;$i<4; $i++)
-       {
-          $jsonTempData = array();
-          $jsonTempData['name']         = $word;
-          $jsonTempData['number']       = $word;
-          $jsonTempData['date_added']   = $word;
-           
-          $jsonData[] = $jsonTempData;
-       }
-     
-     $outputArr = array();
-     $outputArr['Android'] = $jsonData;
-      
-     // Encode Array To JSON Data
-     print_r( json_encode($outputArr));
-      
- 
- ?>
+<?php
+$word = "Hello world!";
+print $word;
+?>
