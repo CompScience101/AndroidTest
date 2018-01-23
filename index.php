bb5d2d77028623a130c8a82bbc1e8d1ec58081ff
CompScience101 <35551293+CompScience101@users.noreply.github.com>
2018-01-23 01:33:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     // Purpose: built as hello world for OOP in PHP, test on Openshift.
     // reference: https://stackoverflow.com/questions/20603992/object-oriented-php-class-simple-example
 
-    //include_once("php_class_object.php");
+    include_once("php_class_object.php");
     //$name = $_POST['name'];   //add name attribute to input tag in HTML
     $name = "testing";
     $myName = new Name();
