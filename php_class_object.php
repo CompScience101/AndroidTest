0bbac55a957345480b6dba7a86cae916469161de
CompScience101 <35551293+CompScience101@users.noreply.github.com>
2018-01-23 00:57:53 -0500

    Create php_class_object.php

---
+++ php_class_object.php
@@ -0,0 +1,24 @@
+<?php
+    // Purpose: built as hello world for OOP in PHP, test on Openshift.
+    // reference: https://stackoverflow.com/questions/20603992/object-oriented-php-class-simple-example
+    // PHP class filename: php_class_object.php
+
+class Name
+{
+    public $name;
+    public function showName()
+    {
+        /**
+        Put your database code here to extract from database.
+        **/
+        return($this->name);
+    }
+    public function enterName($TName)
+    {
+        $this->name = $TName;
+        /**
+        Put your database code here.
+        **/
+    }
+}
+?>
