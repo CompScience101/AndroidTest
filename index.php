10446ad6af049917511e3fbe03d30056d4293cb9
CompScience101 <35551293+CompScience101@users.noreply.github.com>
2018-01-25 04:15:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,5 @@
 <?php
-    // Purpose: built as hello world for OOP in PHP, test on Openshift.
-    // reference: https://stackoverflow.com/questions/20603992/object-oriented-php-class-simple-example
-
-    include_once("php_class_object.php");
-    //$name = $_POST['name'];   //add name attribute to input tag in HTML
-    $name = "testing mysql://mysql";
-    $myName = new Name();
-    $myName->enterName($name); //to save in database/
-    $name=$myName->showName(); //to retrieve from database. 
-    print($name);
     
-    // Show all information, defaults to INFO_ALL
-    //phpinfo();
-
-    //Communicate with Database
-    $servername = "mysql://mysql"; //mysql, 172.30.172.210, jdbc:mysql://mysql:3306/,android-test-53-wn9xb, getenv(MYSQL_PORT_3306_TCP_ADDR) //ip address, getenv("HOSTNAME") //ip address
-    $username = "my_admin";
-    $password = "iversoncarter153";//root
-    $dbname = "AndroidTestDB";
-    $port = getenv("MYSQL_SERVICE_PORT_MYSQL");// default= "3306"
-
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
-
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
     echo "Connected successfully";
     
 ?>
