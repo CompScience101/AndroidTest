9940fc2d7e9bbb4c384dbefcb51c246bd324b021
CompScience101 <35551293+CompScience101@users.noreply.github.com>
2018-01-26 02:34:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 // Pear Mail Library
 require_once "Mail.php";
-
+include_once("php_class_object.php");
+$server_info = new info();
 $from = '<UHC_Tech_Supprt@gmail.com>';
 $to = '<nsdonaldwebmail@gmail.com>'; // '<$_POST['data']>'; //add name attribute to input tag in HTML
 $subject = ''.$_POST['data'].'';
@@ -14,10 +15,10 @@ $headers = array(
 );
 
 $smtp = Mail::factory('smtp', array(
-        'host' => 'ssl://smtp.gmail.com',
-        'port' => '465',
-        'auth' => true,
-        'username' => 'UHC_Tech_Supprt@gmail.com',
+        'host' => ''.$server_info->host.'',
+        'port' => ''.$server_info->port.'',
+        'auth' => ''.$server_info->auth.'',
+        'username' => ''.$server_info->server_support.'',
         'password' => 'mnbiywqacztkgbyf'
     ));
 
