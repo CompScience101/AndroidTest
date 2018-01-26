a639d7212bcee9f977f60fad2b3b31335eedef99
CompScience101 <35551293+CompScience101@users.noreply.github.com>
2018-01-26 03:59:59 -0500

    Update php_class_object.php

--- php_class_object.php
+++ php_class_object.php
@@ -5,10 +5,10 @@
 
 class info
 {
-    private $serverSupport;
-    private $host;
-    private $port;
-    private $auth;
+    $serverSupport;
+    $host;
+    $port;
+    $auth;
     function __construct(){
         $this->serverSupport = '<UHC_Tech_Supprt@gmail.com>';
         $this->host = 'ssl://smtp.gmail.com';
