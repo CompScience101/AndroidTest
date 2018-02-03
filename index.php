bb5a24340334fe4c56eae6b0dfcbac44cc0e8806
CompScience101 <35551293+CompScience101@users.noreply.github.com>
2018-02-03 15:26:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,4 @@
-<?php
-// Pear Mail Library
-require_once "Mail.php";
-include_once("php_class_object.php");
-$server_info = new info();
-$server_info->myInit();
-$from = ''.$server_info->server_support.'';
-$to = '<nsdonaldwebmail@gmail.com>'; // '<$_POST['data']>'; //add name attribute to input tag in HTML
-$subject = ''.$_POST['data'].'';
-$body = "Hi,How are you?";
-
-$headers = array(
-    'From' => $from,
-    'To' => $to,
-    'Subject' => $subject
-);
-
-$smtp = Mail::factory('smtp', array(
-        'host' => ''.$server_info->host.'',
-        'port' => ''.$server_info->port.'',
-        'auth' => ''.$server_info->auth.'',
-        'username' => ''.$server_info->server_support.'',
-        'password' => 'mnbiywqacztkgbyf'
-    ));
-
-$mail = $smtp->send($to, $headers, $body);
-
-if (PEAR::isError($mail)) {
-    echo('<p>' . $mail->getMessage() . '</p>');
-} else {
-    echo('<p>Message successfully sent!</p>');
-}
+<?php 
+echo "testingg php...<br><br>"
+phpinfo();
 ?>
